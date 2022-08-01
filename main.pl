798f561ecba09b0b9ce46a1c494e7b47dc5d057f
LittleJian <chenhowe0529@gmail.com>
2022-08-01 16:22:26 +0800

    add type restrictions for arguments

--- main.pl
+++ main.pl
@@ -8,33 +8,40 @@
 %%     | succ e
 %%     | e \in T
 
-make_var(Name, var(Name)).
-make_lam(Name, Body, lam(Name, Body)).
-make_app(Fun, Arg, app(Fun, Arg)).
-make_zero(zero).
-make_succ(Num, succ(Num)).
-make_ann(Expr, Type, ann(Expr, Type)).
+expr(var(Name)) :- atom(Name).
+expr(lam(Name, Body)) :- atom(Name), expr(Body).
+expr(app(Fun, Arg)) :- expr(Fun), expr(Arg).
+expr(zero).
+expr(succ(Num)) :- expr(Num).
+expr(ann(Expr, Type)) :- expr(Expr), type(Type).
 
 
 %% Here's the definition of the type of the language.
 %% t ::= nat | arrow(t, t)
 
-make_nat(nat).
-make_arrow(Arg, Ret, arrow(Arg, Ret)).
+type(nat).
+type(arrow(Arg, Ret)) :- type(Arg), type(Ret).
 
 % Definitions end.
 
-type_of(Context, zero, nat).
-type_of(Context, succ(Num), nat) :- type_of(Context, Num, nat).
+type_of(Expr, Type) :- 
+    expr(Expr),
+    type_of_helper([], Expr, Type),
+    type(Type). % I have to put type avaliable check here, 
+                % otherwise it will cause some segfault.
 
-type_of(Context, var(Name), Type) :- member([Name, Type], Context).
+type_of_helper(Context, zero, nat).
+type_of_helper(Context, succ(Num), nat) :- type_of_helper(Context, Num, nat).
 
-type_of(Context, app(Fun, Arg), TypeRes) :-
-    type_of(Context, Arg, TypeArg),
-    type_of(Context, Fun, arrow(TypeArg, TypeRes)).
+type_of_helper(Context, var(Name), Type) :- member([Name, Type], Context).
 
-type_of(Context, lam(Name, Body), arrow(TypeArg, TypeRes)) :-
+type_of_helper(Context, app(Fun, Arg), TypeRes) :-
+    type_of_helper(Context, Arg, TypeArg),
+    type_of_helper(Context, Fun, arrow(TypeArg, TypeRes)).
+
+type_of_helper(Context, lam(Name, Body), arrow(TypeArg, TypeRes)) :-
     append([[Name, TypeArg]], Context, NewContext),
-    type_of(NewContext, Body, TypeRes).
+    type_of_helper(NewContext, Body, TypeRes).
 
-type_of(Context, ann(Expr, Type), Type) :- type_of(Context, Expr, Type).
+type_of_helper(Context, ann(Expr, Type), Type) :- 
+    type_of_helper(Context, Expr, Type).
